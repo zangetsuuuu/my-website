feb4d6f4787beb87bf41392346f747ddbb73fad1
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-10 15:58:46 +0700

    feat: add Tailwind CSS framework

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#111111',
+        white: '#F3F3F3',
+        gray: '#888888'
+      }
+    },
+  },
+  plugins: [],
+}
