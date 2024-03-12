55bf20158f43ac84ddf7d39d2270704c3f8443d9
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-12 23:32:11 +0700

    feat: add fixed navbar and offcanvas navbar script

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,20 @@
 // Fixed Navbar
 window.onscroll = function() {
     const header = document.querySelector('header');
+    const offcanvas = document.getElementById('offcanvas-menu');
     const fixedNav = header.offsetTop;
 
     if (window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
+        offcanvas.classList.add('offcanvas-fixed');
     } else {
         header.classList.remove('navbar-fixed');
+        offcanvas.classList.remove('offcanvas-fixed');
     }
 };
+
+// Offcanvas navbar
+document.getElementById("menu-toggle").addEventListener("change", function() {
+    const offcanvas = document.getElementById("offcanvas-menu");
+    offcanvas.classList.toggle("hidden");
+});
